db4323cf090355212c1a021726ecc3d720e7c462
CSDotNET <mrapplefruit0211@gmail.com>
2022-10-03 11:29:21 +0900

    完成

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,23 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using Fumen;
+﻿using Fumen;
 using System.Text;
 
-var url = "v115@khA8JeAgWDAmS8CAvhAAAPAA";
+var url = "";
 
 while (true)
 {
     Console.WriteLine("input");
     url = Console.ReadLine();
 
-    //        "v115@vhAgWDAmS8CA"
-    var decode = FumenParser.Decode(url);
+ var decode = FumenParser.Decode(url);
     var encode = FumenParser.Encode(decode);
     Console.WriteLine(FumenParser.Poll(2, "Je")/240);
     Console.WriteLine("ori:"+url.Replace("?",""));
     Console.WriteLine("enc:"+encode);
-    Console.WriteLine(decode.Pages[0].Comment);
 
     var decode2 = FumenParser.Decode(encode);
     var encode2 = FumenParser.Encode(decode2);
@@ -26,7 +21,6 @@ while (true)
         Console.WriteLine("true");
     else
         Console.WriteLine("false");
-    Console.WriteLine(decode2.Pages[0].Comment);
 
     Console.WriteLine();
 
