75b1d6a365d6ef673fd584d7bfec993f833a67fc
CSDotNET0211 <59834175+CSDotNET0211@users.noreply.github.com>
2022-10-05 06:55:41 +0900

    a

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,23 +5,23 @@ var url = "";
 
 while (true)
 {
-    var test=0;
+    var test = 0;
     //Console.WriteLine(FumenParser.DecodeComment("DAFLrAA", ref test));
 
 
-   // Console.WriteLine(FumenParser.PollRevert(2,8*220+219));
+    // Console.WriteLine(FumenParser.PollRevert(2,8*220+219));
     Console.WriteLine("input");
     url = Console.ReadLine();
 
- var decode = FumenParser.Decode(url);
+    var decode = FumenParser.Decode(url);
     var encode = FumenParser.Encode(decode);
-    Console.WriteLine("ori:"+url.Replace("?",""));
-    Console.WriteLine("enc:"+encode);
+    Console.WriteLine("ori:" + url.Replace("?", ""));
+    Console.WriteLine("enc:" + encode);
 
     var decode2 = FumenParser.Decode(encode);
     var encode2 = FumenParser.Encode(decode2);
-    Console.WriteLine("en2:"+encode2);
-    if (url.Replace("?", "") == encode&&encode == encode2)
+    Console.WriteLine("en2:" + encode2);
+    if (url.Replace("?", "") == encode && encode == encode2)
         Console.WriteLine("true");
     else
         Console.WriteLine("false");
